073010629a0fb35b1cd6a28842841cbb2e118729
Seth Battis <>
2025-08-06 12:50:53 -0400

    refactor: restore slim-skeleton action objects

--- src/Application/Handlers/HttpErrorHandler.php
+++ src/Application/Handlers/HttpErrorHandler.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Application\Handlers;
 
-use GrotonSchool\Slim\Actions\ActionError;
-use GrotonSchool\Slim\Actions\ActionPayload;
+use App\Application\Actions\ActionError;
+use App\Application\Actions\ActionPayload;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpException;
